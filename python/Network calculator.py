4aa2c565aa8ff3e721a923b7982fd9289f38b457
ingeb0rga <jetprice@gmail.com>
2022-04-24 23:31:43 +0300

    Network calculator ready

--- python/Network calculator.py
+++ python/Network calculator.py
@@ -12,13 +12,53 @@ def net_calc(ip, prefix):
     mask_int = [int(mask_bin[x], 2) for x in range(len(mask_bin))]
     net_id_bin = [bin(int(ip_in_bin[x], 2) & int(mask_bin[x], 2)) for x in range(len(ip_in))]
     net_id_int = [int(net_id_bin[x], 2) for x in range(len(net_id_bin))]
+    next_net_id_bin = [bin(int(ip_in_bin[x], 2) & int(mask_bin[x], 2)) for x in range(len(ip_in))]
+    first_host_ip_bin = net_id_bin
     
-    #print(ip_in)
-    #print(ip_in_bin)
-    #print(prefix)
-    #print(mask_bin)
-    #print(net_id_bin)
-    print(mask_int)
-    print("Network_ID:", *net_id_int, sep='.', end='\n')
-    print("Mask:", *mask_int, sep='.', end='\n')
-net_calc(input(), int(input()))
+    wildcard_mask = ''
+    for i in range(len(mask)):
+        if int(mask[i]) == 0:
+            wildcard_mask += '1'
+        else:
+            wildcard_mask += '0'
+    wildcard_mask_bin = [f'0b{int(wildcard_mask[i:i + 8])}' for i in range(0, len(wildcard_mask), 8)]
+
+    broadcast_bin = [bin(int(net_id_bin[x], 2) | int(wildcard_mask_bin[x], 2)) for x in range(len(ip_in))]
+    broadcast_int = [int(broadcast_bin[x], 2) for x in range(len(broadcast_bin))]
+    last_host_ip_bin = broadcast_bin
+    last_host_ip_bin[len(last_host_ip_bin) - 1] = bin(int(last_host_ip_bin[len(last_host_ip_bin) - 1], 2) - 1)
+    last_host_ip_int = [int(last_host_ip_bin[x], 2) for x in range(len(last_host_ip_bin))]
+
+    for i in range(len(mask_bin)):
+        empty_term = [0, 'b', 0, 0, 0, 0, 0, 0, 0, 0]
+        if int(mask_bin[i], 2) == 0:
+            for j in range(len(net_id_bin[i - 1]) - 1, 1, -1):
+                if int(mask_bin[i - 1][j]) == 1:
+                    empty_term[j] = 1
+                    break
+            next_net = bin(int(next_net_id_bin[i - 1], 2) + int(str(''.join(list(map(str, empty_term)))), 2))
+            next_net_id_bin[i - 1] = next_net
+            first_host_ip_bin[i] = bin(int(first_host_ip_bin[i], 2) + 1)
+        
+    next_net_id_int = [int(next_net_id_bin[x], 2) for x in range(len(next_net_id_bin))]
+    first_host_ip_int = [int(first_host_ip_bin[x], 2) for x in range(len(first_host_ip_bin))]
+
+    print("---------------------------------------------")
+    print('Network_ID: ', end='' )
+    print(*net_id_int, sep='.', end='/')
+    print(prefix)
+    print('Mask: ', end='' )
+    print(*mask_int, sep='.', end='\n')
+    print('Next Network_ID: ', end='' )
+    print(*next_net_id_int, sep='.', end='/')
+    print(prefix)
+    print('Broadcast IP: ', end='' )
+    print(*broadcast_int, sep='.', end='\n')
+    print('First Host IP: ', end='' )
+    print(*first_host_ip_int, sep='.', end='\n')
+    print('Last Host IP: ', end='' )
+    print(*last_host_ip_int, sep='.', end='\n')
+    print("IP addresses #:", (2 ** (32 - prefix)) - 2)
+    print("---------------------------------------------")
+        
+net_calc(input("Enter IP: "), int(input("Enter network prefix: ")))
--- python/temp3.py
+++ python/temp3.py
@@ -1,8 +1,25 @@
-a = '0b11000000'
-b = '0b11111111'
-integer_sum = int(a, 2) + int(b, 2)
-binary_sum = bin(integer_sum)
-binary_and = bin(int(a, 2) & int(b, 2))
-print(integer_sum)
-print(binary_sum)
-print(binary_and)
+a = '0b10011100'
+a = str(a)
+b = [0, 'b', 0, 0, 0, 0, 0, 0, 0, 0]
+for i in range(len(a) - 1, 1, -1):
+    if int(a[i]) == 1:
+        b[i] = 1
+        break
+
+print(a)
+print(b)
+c = str(''.join(list(map(str, b))))
+#print(c)
+sum = bin(int(a, 2) + int(c, 2))
+print(sum)
+print(int(sum, 2))
+#print(bin(int(a, 2)))
+#print(bin(int(c, 2)))
+
+
+#integer_sum = int(a, 2) + int(b, 2)
+#binary_sum = bin(integer_sum)
+#binary_and = bin(int(a, 2) & int(b, 2))
+#print(integer_sum)
+#print(binary_sum)
+#print(binary_and)
