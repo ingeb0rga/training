96250ea4b0b377a8fe1ff96a610c9aa2d027c807
ingeb0rga <jetprice@gmail.com>
2022-05-29 21:23:35 +0300

    powershell devops training

---
+++ powershell/10 words.ps1
@@ -0,0 +1,16 @@
+﻿$text = (Get-Content C:\temp\txt.txt).ToLower()
+$words = $text.Split()
+$hash = @{}
+
+foreach ($word in $words) {
+    if ($word -match "$null") {
+        if ($hash.ContainsKey($word)) {
+            $hash[$word] += 1
+        }
+        else {
+            $hash.Add($word, 1)
+        }
+    }
+}
+
+$hash.GetEnumerator() | Sort-Object -Property Value -Descending | select -First 10
---
+++ powershell/bubble sort.ps1
@@ -0,0 +1,34 @@
+﻿$var = Read-Host "Verbose? (y/n)"
+if ($var -match 'y|yes|Yes|Y|YES') {
+    $VerbosePreference = 'Continue'
+}
+elseif ($var -match 'n|no|N|NO') {
+    $VerbosePreference = 'SilentlyContinue'
+}
+
+$list = @()
+for ($i=1; $i -le 20; $i++) {
+    $list += Get-Random -Minimum 1 -Maximum 100
+}
+
+Write-Host
+Write-Host "Initial list:"
+foreach ($s in $list) {Write-Host "$s " -NoNewline}
+Write-Host
+
+for ($a=1; $a -lt $list.Length; $a++) {
+    for ($b=0; $b -lt ($list.Length - 1); $b++) {
+        if ($list[$b] -ge $list[$b + 1]) {
+            $temp = $list[$b]
+            $list[$b] = $list[$b + 1]
+            $list[$b + 1] = $temp
+        }
+        Write-Verbose "$list"
+        
+    }
+}
+
+Write-Host
+Write-Host "Bubble sorted list:"
+foreach ($s in $list) {Write-Host "$s " -NoNewline -Verbose}
+Write-Host
