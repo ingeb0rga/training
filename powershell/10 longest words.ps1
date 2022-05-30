0a2ff60dff12c58e19aaed015d9877062128dff1
ingeb0rga <jetprice@gmail.com>
2022-05-31 00:20:31 +0300

    powershell devops training

---
+++ powershell/10 longest words.ps1
@@ -0,0 +1,10 @@
+﻿$text = (Get-Content C:\temp\txt.txt).ToLower()
+$words = $text.Split().ToLower().Replace(',','').Replace('.','') | sort -Unique
+$hash = @{}
+
+foreach ($word in $words) {
+    if ($hash.ContainsKey($word) ) {continue}
+    else {$hash.Add($word, $word.Length)}
+}
+
+$hash.GetEnumerator() | Sort-Object -Property Name | Sort-Object -Property Value -Descending | select -First 10 > C:\temp\10_longest_words.txt
