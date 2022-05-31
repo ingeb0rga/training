4f62d5b3edbfb7e79188670ed345e46333406062
ingeb0rga <jetprice@gmail.com>
2022-05-31 16:01:31 +0300

    powershell devops training

--- powershell/format converter.ps1
+++ powershell/format converter.ps1
@@ -1,23 +1,22 @@
 ﻿$processes = Get-Process -Name "chrome" | Select-Object Name, Id, StartTime, PagedMemorySize
+#Создаем hashtable из массива
 $hash = foreach ($process in $processes) { @{$processes.IndexOf($process) = @{Name = "$($process.Name)"; Id = "$($process.Id)"; StartTime = "$($process.StartTime)"; PagedMemorySize = "$($process.PagedMemorySize)"} } }
-
+#Сохраняем массив в .csv
 $processes | ConvertTo-Csv | Set-Content -Path .\demo.csv
-
+#Сохраняем массив в .json
 $processes | ConvertTo-Json | Set-Content -Path .\demo.json
-
-#$xml = [xml]$($processes | ConvertTo-Xml -Depth 2)
-#$xml.Save( $(Resolve-Path .\).Path + ".\demo.xml" )
+#Типизируем и сохраняем массив в .xml
 ([xml]$($processes | ConvertTo-Xml -Depth 2)).Save( $(Resolve-Path .\).Path + ".\demo.xml" )
-
+#Сохраняем массив в .yaml
 ConvertTo-Yaml -Data $hash -OutFile $($(Resolve-Path .\).Path + ".\demo.yaml") -Force
-
+#Читаем .csv и создаем hashtable
 $csvdata = Get-Content -Path .\demo.csv | ConvertFrom-Csv
 $csvhash = foreach ($line in $csvdata) { @{$csvdata.IndexOf($line) = @{Name = "$($line.Name)"; Id = "$($line.Id)"; StartTime = "$($line.StartTime)"; PagedMemorySize = "$($line.PagedMemorySize)"} } }
-
+#Читаем .json и создаем hashtable
 $jsondata = Get-Content -Path .\demo.json | ConvertFrom-Json
 $jsonhash = foreach ($line in $jsondata) { @{$jsondata.IndexOf($line) = @{Name = "$($line.Name)"; Id = "$($line.Id)"; StartTime = "$($line.StartTime)"; PagedMemorySize = "$($line.PagedMemorySize)"} } }
-
+#Читаем .xml и создаем hashtable
 $xmldata = [xml]$(Get-Content -Path .\demo.xml)
 $xmlhash = foreach ($line in $xmldata.Objects.Object) { @{$xmldata.Objects.Object.IndexOf($line) = @{Name = "$($line.Name)"; Id = "$($line.Id)"; StartTime = "$($line.StartTime)"; PagedMemorySize = "$($line.PagedMemorySize)"} } }
-
+#Читаем .yaml hashtable
 $yamlhash = ConvertFrom-Yaml -Yaml $( $(Get-Content .\demo.yaml) -join "`n" )
