260605c4b55b6ba759ebd119a3fc4cdebe3de880
ingeb0rga <jetprice@gmail.com>
2022-06-01 14:19:49 +0300

    powershell devops training

--- powershell/CIM and WMI.ps1
+++ powershell/CIM and WMI.ps1
@@ -1,14 +1,27 @@
 ﻿#CPU model and max clock speed
 Get-CimInstance -ComputerName localhost -ClassName Win32_Processor | Select Name, MaxClockSpeed
+
 #Amount of RAM (total, free)
 Get-CIMInstance -ComputerName localhost -ClassName Win32_OperatingSystem | Select FreePhysicalMemory, TotalVisibleMemory
+
 #OS disk free space
 Get-CimInstance -ComputerName localhost -ClassName Win32_LogicalDisk | Select Size, FreeSpace
+
 #OS version
 Get-CIMInstance -ComputerName localhost -ClassName Win32_OperatingSystem | Select Caption, Version
+
 #Installed hot fixes
-#Get-CIMInstance -ClassName Win32_QuickFixEngineering
-Get-CIMInstance -ComputerName locahost -ClassName Win32_QuickFixEngineering
+Get-CIMInstance -ClassName Win32_QuickFixEngineering
+#Get-CIMInstance -ComputerName locahost -ClassName Win32_QuickFixEngineering
+
 #List of stopped services
-#Get-CIMInstance -ClassName Win32_Service | Where-Object {$_.State -eq "Stopped"}
-Get-CimInstance -ComputerName localhost -Query "SELECT * FROM win32_service WHERE state='Stopped'"
+Get-CIMInstance -ClassName Win32_Service | Where-Object {$_.State -eq "Stopped"}
+#Get-CimInstance -ComputerName localhost -Query "SELECT * FROM win32_service WHERE state='Stopped'"
+
+#Смотрим залогинившихся пользователей
+Get-WmiObject -ComputerName localhost Win32_LogonSession | ForEach-Object { 
+    [PSCustomObject]@{ 
+    LogonName = $_.GetRelated('Win32_Account').Caption 
+        #SessionStarted = [System.Management.ManagementDateTimeConverter]::ToDateTime($_.StartTime) 
+    } 
+} 
--- powershell/fibbo.ps1
+++ powershell/fibbo.ps1
@@ -1,6 +1,6 @@
 ﻿$N = Read-Host "Введите значение N"
-$temp = @(1, 1)
-$fib = @(1, 1)
+$temp = @(1, 0)
+$fib = @(0, 1)
 while (($temp[0] + $temp[1]) -lt $N) {
     $a = $temp[0]
     $temp[0] = $temp[0] + $temp[1]
