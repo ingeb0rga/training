347ce371b3611e80041be3a20c3e3a8c0f3b1c75
ingeb0rga <jetprice@gmail.com>
2022-06-06 17:16:27 +0300

    DevOps Fundamentals exit tasks

---
+++ Devops Fundamentals/Exit Tasks/task1.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Checking for input file in positional argument. Exit with help message, if no input file
+if [ -z $@  ]; then
+  echo "Error! Please use the script with a path to the input file (eg: ./task1.sh ./accounts.csv)"
+  exit 1
+fi
+
+# Replacing symbol combinations, that interfere to normal columns splitting, and assign the result to variable
+temp_file=$(cat $* | sed -e 's|, |___|g')
+
+# Array of "id" column
+readarray -t arr_id < <(echo "$temp_file" | tail -n +2 | cut -d ',' -f1)
+# Array of "location_id" column
+readarray -t arr_l_id < <(echo "$temp_file" | tail -n +2 | cut -d ',' -f2)
+# Aarray of "name" column, formatted as required ("Name Sirname")
+readarray -t arr_name < <(echo "$temp_file" | tail -n +2 | cut -d ',' -f3 | sed -e "s/\b\(.\)/\u\1/g")
+# Array of "title" column
+readarray -t arr_title < <(echo "$temp_file" | tail -n +2 | cut -d ',' -f4)
+# Array of "department" column
+readarray -t arr_dep < <(echo "$temp_file" | tail -n +2 | cut -d ',' -f6)
+
+# Filling array of "email" column with emails formatted as required
+for (( i=0; i<${#arr_name[@]}; i++ )); do
+# Fist letter of name to lowercase
+  first=$(echo ${arr_name[$i]:0:1} | awk '{print tolower($0)}')
+# Sirname to lowercase
+  sirname=$(echo ${arr_name[$i]} | cut -d ' ' -f2 | awk '{print tolower($0)}')
+#  Adding values into array of "email" column
+  arr_email+=( "$first$sirname" )	# Concatenating email (without adding domain and checking for coincidences) and adding to array
+done
+
+# Checking if emails are unique.
+# arr_uniq is a temporary associated array of emails and attributes of uniqueness.
+# If email is unique, it is assigned the value "1", if not unique - the value "2".
+declare -A arr_uniq
+for email in "${arr_email[@]}"; do
+  if [ -z ${arr_uniq[$email]} ]; then
+	arr_uniq[$email]=1
+  else
+	arr_uniq[$email]=2
+  fi
+done
+
+# Formatting emails as required, adding location_id (where needed) and domain
+for (( i=0; i<(${#arr_email[@]}); i++ )); do
+  email=${arr_email[$i]}
+# Testing if email is unique
+  if [[ ${arr_uniq[$email]} -eq 2 ]]; then
+	arr_email[$i]="${arr_email[$i]}${arr_l_id[$i]}@abc.com"
+  else 
+	arr_email[$i]="${arr_email[$i]}@abc.com"
+  fi
+done
+
+# Formattinging the path to the converted file
+new_file=$(echo $* | sed -e 's|accounts|accounts_new|g')
+
+# Importing line with column titles into converted file
+head -n 1 $* > $new_file
+
+# Importing converted data per line into converted file
+for (( i=0; i<${#arr_id[@]}; i++ )); do
+  echo "${arr_id[$i]},${arr_l_id[$i]},${arr_name[$i]},${arr_title[$i]},${arr_email[$i]},${arr_dep[$i]}" >> $new_file
+done
+
+# Returning the peplaced symbol combinations, that interfered to normal columns splitting
+sed -i 's|___|, |g' $new_file
