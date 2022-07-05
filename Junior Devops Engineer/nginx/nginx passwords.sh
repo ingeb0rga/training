aa4e689c765c9f8a11d6b62680f89d6a170c2e47
ingeb0rga <jetprice@gmail.com>
2022-07-05 08:56:05 +0300

    ansible training

---
+++ Junior Devops Engineer/nginx/nginx passwords.sh
@@ -0,0 +1,21 @@
+﻿# Create a password for securing locations
+# apt-get install -y apache2-utils 	
+htpasswd -b -c /etc/nginx/passwords admin admin
+chown www-data /etc/nginx/passwords
+chmod 600 /etc/nginx/passwords
+
+location /appointments/ {
+# only allow IPs from the same network the server is on
+	allow 192.168.0.0/24;
+	allow 10.0.0.0/8;
+	deny all;
+
+	auth_basic "Authentication is required...";
+	auth_basic_user_file /etc/nginx/passwords;
+
+	location ~ \.php$ {
+	include snippets/fastcgi-php.conf;
+	fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
+	fastcgi_intercept_errors on;
+	}
+}
---
+++ Junior Devops Engineer/nginx/openssl certificate.sh
@@ -0,0 +1,25 @@
+﻿# Create an SSL key and certificate
+# apt -y install openssl
+openssl req -batch -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt 2>/dev/null
+
+server {
+	listen 443 ssl default_server;
+	ssl_certificate /etc/ssl/certs/nginx.crt;
+	ssl_certificate_key /etc/ssl/private/nginx.key;
+
+	root /var/www/wisdompetmed.local;
+
+	server_name wisdompetmed.local www.wisdompetmed.local;
+
+	index index.html index.htm index.php;
+
+	access_log /var/log/nginx/wisdompetmed.local.access.log;
+	error_log /var/log/nginx/wisdompetmed.local.error.log;
+
+	location / {
+	# First attempt to serve request as file, then
+	# as directory, then fall back to displaying a 404.
+	try_files $uri $uri/ =404;
+	}
+}
+
