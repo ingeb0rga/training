5964cd70e61ce3b403b6e429642a7d7e24a8e555
ingeb0rga <jetprice@gmail.com>
2022-09-30 18:56:13 +0300

    polished bubble_sort

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -5,11 +5,11 @@ stop = int(input("Enter max number: "))
 limit = int(input("Enter the length of the list: "))
 
 my_list = [random.randrange(start, stop) for i in range(limit)]
-# print(my_list)
+print(my_list)
 
 def bubble_sort(list):
     for k in range(len(my_list) - 1):
-        for i in range(len(my_list) - 1):
+        for i in range(len(my_list) - 1 - k):
             if my_list[i] > my_list[i + 1]:
                 my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
     print(my_list)
