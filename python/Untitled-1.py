25dc0fcb31a565d9c529eb42a5ef367010e2bcf0
ingeb0rga <jetprice@gmail.com>
2022-10-30 20:51:58 +0300

    k8s

--- python/Untitled-1.py
+++ python/Untitled-1.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-from datetime import date
-from datetime import time
-# import datetime
+def elapsed_time_in_minutes(layer,time):
+    # return preparation_time_in_minutes(layer) + bake_time_remaining(baking)
+    return (layer * 2) + (40 - time)
 
-# print(datetime.now())
-today = date.today()
-print("%s.%s.%s" % (today.day, today.month, today.year))
+print(elapsed_time_in_minutes(1, 3))
