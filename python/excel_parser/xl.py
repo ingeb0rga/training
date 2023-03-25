70ac8a4b845c38401d280b674733a19345568d92
ingeb0rga <jetprice@gmail.com>
2023-03-26 00:35:42 +0300

    modified excel parser, removed .exe

--- python/excel_parser/xl.py
+++ python/excel_parser/xl.py
@@ -1,42 +1,49 @@
 import openpyxl
 
-wb = openpyxl.load_workbook(filename='./sf.xlsx')
-sheet_sf = wb['TDSheet']
-sheet_gtd = wb['gtd']
+wb = openpyxl.load_workbook(filename="./sf.xlsx")
+sheet_sf = wb["TDSheet"]
+sheet_gtd = wb["gtd"]
 wb.active = sheet_sf
 
-def main():
-    goods = {}
-    for row in range(1, sheet_gtd.max_row + 1):
-        if sheet_gtd[f'B{row}'].value.strip() in goods:
-            goods[sheet_gtd[f'B{row}'].value.strip()] += 1
-        else:
-            goods[sheet_gtd[f'B{row}'].value.strip()] = 1
 
-    uniq_goods = goods.copy()
+def main():
+    goods = {}  # Dict of all goods with number of repeatences in gtd sheet
+    for row in range(1, sheet_gtd.max_row + 1):
+        if sheet_gtd[f"B{row}"].value.strip() in goods:
+            goods[sheet_gtd[f"B{row}"].value.strip()] += 1
+        else:
+            goods[sheet_gtd[f"B{row}"].value.strip()] = 1
+
+    uniq_goods = goods.copy()   # Dict of uniq goods in gtd sheet
     for k in goods:
         if goods[k] > 1:
             del uniq_goods[k]
 
-    for row in range(17, sheet_sf.max_row):
-        # if sheet_sf[f'B{row}'].value == "Всего к оплате":
-        if "Всего к оплате" in sheet_sf[f'B{row}'].value:
-            sheet_sf_end = row
+    dict_gtd = {}   # Dict of uniq goods in gtd sheet with gtds as values
+    for row in range(1, sheet_gtd.max_row + 1):
+        if sheet_gtd[f"B{row}"].value.strip() in uniq_goods:
+            dict_gtd[sheet_gtd[f"B{row}"].value.strip()] = sheet_gtd[f"E{row}"].value.strip()
+
+    for row in range(1, sheet_sf.max_row):
+        if sheet_sf[f"B{row}"].value == 1:
+            sheet_sf_begin = row + 1   # The first line of goods list in TDSheet sheet
+            print(sheet_sf_begin) 
             break
 
-    dict_gtd = {}
-    for row in range(1, sheet_gtd.max_row + 1):
-        if sheet_gtd[f'B{row}'].value.strip() in uniq_goods:
-            # dict_gtd[sheet_gtd[f'B{row}'].value.strip()] = {"row": row, "gtd": sheet_gtd[f'E{row}'].value.strip()}
-            dict_gtd[sheet_gtd[f'B{row}'].value.strip()] = sheet_gtd[f'E{row}'].value.strip()
+    for row in range(sheet_sf_begin, sheet_sf.max_row):
+        if "Всего к оплате" in sheet_sf[f"B{row}"].value:
+            sheet_sf_end = row  # The last line of goods list in TDSheet sheet
+            print(sheet_sf_end)
+            break
 
-    for row in range(17, sheet_sf_end):
-        if sheet_sf[f'B{row}'].value.strip() in dict_gtd:
-            # sheet_sf.cell(row=row, column=32).value = dict_gtd[sheet_sf[f'B{row}'].value.strip()]["gtd"]
-            sheet_sf.cell(row=row, column=32).value = dict_gtd[sheet_sf[f'B{row}'].value.strip()]
-    
+    # Insterting gtds from gtd sheet into TDSheet sheet
+    for row in range(sheet_sf_begin, sheet_sf_end):
+        if sheet_sf[f"B{row}"].value.strip() in dict_gtd:
+            sheet_sf.cell(row=row, column=32).value = dict_gtd[sheet_sf[f"B{row}"].value.strip()]
+
     wb.remove(sheet_gtd)
-    wb.save('./sf_gtd.xlsx')
+    wb.save("./sf_gtd.xlsx")
+
 
 if __name__ == "__main__":
     main()
