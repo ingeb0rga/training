c35d8e894ce06a5f1c0966a77574ef08441ff8d4
spirt <spirt>
2023-04-16 13:22:03 +0300

    modified network calculator and excel parser

--- Devops Fundamentals/python/Network calculator.py
+++ Devops Fundamentals/python/Network calculator.py
@@ -1,43 +1,45 @@
-# Функция "Сетевой калькулятор"
+# Network Calculator
 def net_calc(ip, prefix):
     octets = 4
-    bits_per_octet = 10		# Префикс '0b' плюс 8bit
-    
-# Преобразуем IP адрес в двоичный формат
-    ip_in = [int(x) for x in ip.split('.')]
+    bits_per_octet = 10  # Prefix '0b' plus 8bit
+
+    # Convert decimal IP address to binary format
+    ip_in = [int(x) for x in ip.split(".")]
     ip_in_bin = [bin(x) for x in ip_in]
 
-# Маска
-    mask = ''
+    # Mask
+    mask = ""
     for i in range(prefix):
-        mask += '1'
+        mask += "1"
     for j in range(32 - prefix):
-        mask += '0'
-# Маска в двоичном формате
-    mask_bin = [f'0b{int(mask[i:i + 8])}' for i in range(0, len(mask), 8)]
-# Маска в десятичном формате
+        mask += "0"
+    # Binary mask
+    mask_bin = [f"0b{int(mask[i:i + 8])}" for i in range(0, len(mask), 8)]
+    # Decimal mask
     mask_int = [int(mask_bin[x], 2) for x in range(octets)]
-# Обратная маска
-    wildcard_mask = ''        
+    # Wildcard mask
+    wildcard_mask = ""
     for i in range(len(mask)):
         if int(mask[i]) == 0:
-            wildcard_mask += '1'
+            wildcard_mask += "1"
         else:
-            wildcard_mask += '0'
-# Обратная маска в двоичном формате
-    wildcard_mask_bin = [f'0b{int(wildcard_mask[i:i + 8])}' for i in range(0, len(wildcard_mask), 8)]
-# Обратная маска в десятичном формате
+            wildcard_mask += "0"
+    # Binary wildcard mask
+    wildcard_mask_bin = [
+        f"0b{int(wildcard_mask[i:i + 8])}" for i in range(0, len(wildcard_mask), 8)
+    ]
+    # Decimal wildcard mask
     wildcard_mask_int = [int(wildcard_mask_bin[x], 2) for x in range(octets)]
-# Адрес подсети в двоичном формате
+    # Binary subnet addrerss
     net_id_bin = [bin(int(ip_in_bin[x], 2) & int(mask_bin[x], 2)) for x in range(octets)]
-# Адрес подсети в десятичном формате
+    # Decimal subnet address
     net_id_int = [int(net_id_bin[x], 2) for x in range(octets)]
-# Адрес следующей подсети в двоичном формате
+    # Next subnet binary IP
     next_net_id_bin = [bin(int(ip_in_bin[x], 2) & int(mask_bin[x], 2)) for x in range(octets)]
 
-    index = ''
+    index = ""
     for i in range(octets - 1, -1, -1):
-        empty_term = [0, 'b', 0, 0, 0, 0, 0, 0, 0, 0]
+        empty_term = [0, "b", 0, 0, 0, 0, 0, 0, 0, 0]
         if int(mask_bin[i], 2) != 0:
             index = i
             for j in range(bits_per_octet - 1, 1, -1):
@@ -45,45 +47,49 @@ def net_calc(ip, prefix):
                     empty_term[j] = 1
                     break
             break
-    next_net_id_bin[index] = bin(int(next_net_id_bin[i], 2) + int(str(''.join(list(map(str, empty_term)))), 2))
-# Адрес следдующей подсети в десятичном формате
+    next_net_id_bin[index] = bin(
+        int(next_net_id_bin[i], 2) + int(str("".join(list(map(str, empty_term)))), 2)
+    )
+    # Next subnet decimal IP
     next_net_id_int = [int(next_net_id_bin[x], 2) for x in range(octets)]
-# Широковещательный адрес в двоичном формате
-    broadcast_bin = [bin(int(net_id_bin[x], 2) | int(wildcard_mask_bin[x], 2)) for x in range(octets)]
-# Широковещательный адрес в десятичном формате
+    # Binary broacast address
+    broadcast_bin = [
+        bin(int(net_id_bin[x], 2) | int(wildcard_mask_bin[x], 2)) for x in range(octets)
+    ]
+    # Decimal broacast address
     broadcast_int = [int(broadcast_bin[x], 2) for x in range(octets)]
-# Адрес последнего хоста в двоичном формате
+    # Last host binary IP
     last_host_ip_bin = [broadcast_bin[x] for x in range(octets)]
     last_host_ip_bin[octets - 1] = bin(int(last_host_ip_bin[octets - 1], 2) - 1)
-# Адрес последнего хоста в десятичном формате
+    # Last host decimal IP
     last_host_ip_int = [int(last_host_ip_bin[x], 2) for x in range(octets)]
-# Адрес первого хоста в двоичном формате   
+    # First host binary IP
     first_host_ip_bin = [x for x in net_id_bin]
     first_host_ip_bin[octets - 1] = bin(int(first_host_ip_bin[octets - 1], 2) + 1)
-# Адрес первого хоста в десятичном формате
+    # First host decimal IP
     first_host_ip_int = [int(first_host_ip_bin[x], 2) for x in range(octets)]
 
     print("---------------------------------------------")
-    print('Network_ID:\t\t', end='')
-    print(*net_id_int, sep='.', end='/')
+    print("Network_ID:\t\t", end="")
+    print(*net_id_int, sep=".", end="/")
     print(prefix)
-    print('Mask:\t\t\t', end='')
-    print(*mask_int, sep='.', end='\n')
-    print('Wildcart:\t\t', end='')
-    print(*wildcard_mask_int, sep='.', end='\n')
-    print('Next Network_ID:\t', end='')
-    print(*next_net_id_int, sep='.', end='/')
+    print("Mask:\t\t\t", end="")
+    print(*mask_int, sep=".", end="\n")
+    print("Wildcart:\t\t", end="")
+    print(*wildcard_mask_int, sep=".", end="\n")
+    print("Next Network_ID:\t", end="")
+    print(*next_net_id_int, sep=".", end="/")
     print(prefix)
-    print('Broadcast IP:\t\t', end='')
-    print(*broadcast_int, sep='.', end='\n')
-    print('First Host IP:\t\t', end='')
-    print(*first_host_ip_int, sep='.', end='\n')
-    print('Last Host IP:\t\t', end='')
-    print(*last_host_ip_int, sep='.', end='\n')
-    print('IP addresses #:\t\t', end='')
+    print("Broadcast IP:\t\t", end="")
+    print(*broadcast_int, sep=".", end="\n")
+    print("First Host IP:\t\t", end="")
+    print(*first_host_ip_int, sep=".", end="\n")
+    print("Last Host IP:\t\t", end="")
+    print(*last_host_ip_int, sep=".", end="\n")
+    print("IP addresses #:\t\t", end="")
     print(2 ** (32 - prefix) - 2)
     print("---------------------------------------------")
 
-# Вызываем функцию
+
 if __name__ == "__main__":
     net_calc(input("Enter IP: "), int(input("Enter network prefix: ")))
--- python/excel_parser/xl.py
+++ python/excel_parser/xl.py
@@ -1,45 +1,85 @@
 import openpyxl
 
 wb = openpyxl.load_workbook(filename="./sf.xlsx")
-sheet_sf = wb["TDSheet"]
-sheet_gtd = wb["gtd"]
-wb.active = sheet_sf
+sf = wb["TDSheet"]
+gtd = wb["gtd"]
+wb.active = sf
 
 
 def main():
-    goods = {}  # Dict of all goods with number of repeatences in gtd sheet
-    for row in range(1, sheet_gtd.max_row + 1):
-        if sheet_gtd[f"B{row}"].value.strip() in goods:
-            goods[sheet_gtd[f"B{row}"].value.strip()] += 1
+    for row in range(1, sf.max_row):
+        if sf[f"B{row}"].value == 1:
+            sf_begin = row + 1  # The first line of goods list in "TDSheet" sheet
+            break
+
+    for row in range(sf_begin, sf.max_row):
+        if "Всего к оплате" in sf[f"B{row}"].value:
+            sf_end = row  # The last line of goods list in "TDSheet" sheet
+            break
+
+    # Formatting name of goods in "sf" sheet
+    for row in range(sf_begin, sf_end + 1):
+        sf.cell(row=row, column=2).value = (
+            str(" ".join(sf[f"B{row}"].value.split())).split(", Страна", 1)[0].strip()
+        )
+
+    # Formatting name of goods in "gtd" sheet
+    for row in range(1, gtd.max_row + 1):
+        gtd.cell(row=row, column=2).value = str(" ".join(gtd[f"B{row}"].value.split())).strip()
+        if str(gtd[f"E{row}"].value).strip() != "None":
+            gtd.cell(row=row, column=5).value = str(gtd[f"E{row}"].value).strip()
         else:
-            goods[sheet_gtd[f"B{row}"].value.strip()] = 1
+            gtd.cell(row=row, column=5).value = str("-")
 
-    uniq_goods = goods.copy()   # Dict of uniq goods in gtd sheet
-    for k in goods:
-        if goods[k] > 1:
-            del uniq_goods[k]
+    goods = {}  # Dict of all goods with "yes" - uniq and "no" not uniq goods in "gtd" sheet
+    for row in range(1, gtd.max_row + 1):
+        if gtd[f"B{row}"].value not in goods:
+            goods[gtd[f"B{row}"].value] = {"gtd_num": gtd[f"E{row}"].value, "uniq_gtd": "yes"}
+        elif (
+            gtd[f"B{row}"].value in goods
+            and goods[gtd[f"B{row}"].value]["gtd_num"] != gtd[f"E{row}"].value
+        ):
+            goods[gtd[f"B{row}"].value].update({"uniq_gtd": "no"})
 
-    dict_gtd = {}   # Dict of uniq goods in gtd sheet with gtds as values
-    for row in range(1, sheet_gtd.max_row + 1):
-        if sheet_gtd[f"B{row}"].value.strip() in uniq_goods:
-            dict_gtd[sheet_gtd[f"B{row}"].value.strip()] = sheet_gtd[f"E{row}"].value.strip()
+    notuniq_goods_tmp = goods.copy()  # Temporary dict of not uniq goods in "gtd" sheet
+    uniq_goods = goods.copy()  # Dict of uniq goods in "gtd" sheet
+    for val in goods:
+        if goods[val]["uniq_gtd"] == "yes":
+            del notuniq_goods_tmp[val]
+        else:
+            del uniq_goods[val]
 
-    for row in range(1, sheet_sf.max_row):
-        if sheet_sf[f"B{row}"].value == 1:
-            sheet_sf_begin = row + 1   # The first line of goods list in TDSheet sheet
-            break
+    notuniq_goods = {}  # Dict of not uniq goods in "gtd" sheet with gtds and quantities
+    for row in range(1, gtd.max_row + 1):
+        if gtd[f"B{row}"].value in notuniq_goods_tmp:
+            if gtd[f"B{row}"].value not in notuniq_goods:
+                notuniq_goods[gtd[f"B{row}"].value] = {gtd[f"E{row}"].value: gtd[f"D{row}"].value}
+            else:
+                if gtd[f"E{row}"].value not in notuniq_goods[gtd[f"B{row}"].value]:
+                    notuniq_goods[gtd[f"B{row}"].value].update(
+                        {gtd[f"E{row}"].value: gtd[f"D{row}"].value}
+                    )
+                else:
+                    new_val = (
+                        notuniq_goods[gtd[f"B{row}"].value][gtd[f"E{row}"].value]
+                        + gtd[f"D{row}"].value
+                    )
+                    notuniq_goods[gtd[f"B{row}"].value].update({gtd[f"E{row}"].value: new_val})
 
-    for row in range(sheet_sf_begin, sheet_sf.max_row):
-        if "Всего к оплате" in sheet_sf[f"B{row}"].value:
-            sheet_sf_end = row  # The last line of goods list in TDSheet sheet
-            break
+    # Inserting gtds from "gtd" sheet into "TDSheet" sheet
+    for row in range(sf_begin, sf_end):
+        if sf[f"B{row}"].value in uniq_goods:
+            sf.cell(row=row, column=32).value = uniq_goods[sf[f"B{row}"].value]["gtd_num"]
+        else:
+            for key, val in notuniq_goods[sf[f"B{row}"].value].items():
+                if (
+                    notuniq_goods[sf[f"B{row}"].value][key] == sf[f"J{row}"].value
+                    and not sf[f"AF{row}"].value
+                ):
+                    sf.cell(row=row, column=32).value = key
+                    notuniq_goods[sf[f"B{row}"].value].update({key: 0})
 
-    # Insterting gtds from gtd sheet into TDSheet sheet
-    for row in range(sheet_sf_begin, sheet_sf_end):
-        if sheet_sf[f"B{row}"].value.strip() in dict_gtd:
-            sheet_sf.cell(row=row, column=32).value = dict_gtd[sheet_sf[f"B{row}"].value.strip()]
-
-    wb.remove(sheet_gtd)
+    wb.remove(gtd)
     wb.save("./sf_gtd.xlsx")
 
 
